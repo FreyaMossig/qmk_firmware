3b4187cbeec5724431de67b937c3254bfdeba9dc
Freya <f.mossig@gmail.com>
2022-12-25 23:57:50 +0100

    Reorder non-letter keys in base layer

--- keyboards/moonlander/keymaps/FreyaMossig/keymap.c
+++ keyboards/moonlander/keymaps/FreyaMossig/keymap.c
@@ -35,12 +35,12 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_EQL,  KC_1,          KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_DEL,  KC_Q,          KC_W,    KC_E,    KC_R,    KC_T,    TT(SYMB),          TT(SYMB), KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_BSPC, KC_A,          KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Y,          KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_GRV,  WEBUSB_PAIR,   KC_SPC,  KC_LEFT, KC_RGHT,          KC_APP,            KC_ESC,            KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(MDIA),
-                                                  KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,          XXXXXXX,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_EQL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TT(SYMB),         TT(SYMB), KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,          KC_MEH,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_APP,  KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT,          KC_LCTL,          KC_APP,            KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(MDIA),
+                                            KC_TAB,  KC_BSPC, KC_DEL,           KC_ALGR,  KC_ENT,  KC_SPC
     ),
 
     [SYMB] = LAYOUT_moonlander(
