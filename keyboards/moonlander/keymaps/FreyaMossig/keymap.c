03de638bbf1752bfa451fff193cc148ec2b5e591
Freya Moßig <f.mossig@gmail.com>
2026-01-04 01:43:36 +0100

    replaqce TG and MO by tap-toggle (TT) reorder non letter keys and thumb keys

--- keyboards/moonlander/keymaps/FreyaMossig/keymap.c
+++ keyboards/moonlander/keymaps/FreyaMossig/keymap.c
@@ -26,49 +26,44 @@ enum layers {
     BASE,  // default layer
     SYMB,  // symbols
     MDIA,  // media keys
-};
-
-enum custom_keycodes {
-    VRSN = ML_SAFE_RANGE,
+    LEAG,  // League of Legends layer
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_EQL,  KC_1,          KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_DEL,  KC_Q,          KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),          TG(SYMB), KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_BSPC, KC_A,          KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Y,          KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_GRV,  WEBUSB_PAIR,   KC_SPC,  KC_LEFT, KC_RGHT,          KC_APP,            KC_ESC,            KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                                  KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,          TG(LEAG), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_EQL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TT(SYMB),         TT(SYMB), KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,          KC_MEH,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        XXXXXXX, KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT,          KC_LCTL,          KC_APP,            KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(MDIA),
+                                            KC_BSPC,  KC_TAB, KC_DEL,           KC_ALGR,  KC_ENT,  KC_SPC
     ),
-
+ 
     [SYMB] = LAYOUT_moonlander(
-        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
         _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
         _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
         EE_CLR,  _______, _______, _______, _______,          RGB_VAI,           RGB_TOG,          _______, KC_DOT,  KC_0,    KC_EQL,  _______,
-                                            RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,_______, _______
+                                            RGB_HUD, RGB_VAD, RGB_HUI,           TOGGLE_LAYER_COLOR,_______, _______
     ),
-
-    [MDIA] = LAYOUT_moonlander(
+        [MDIA] = LAYOUT_moonlander(
         LED_LEVEL,_______,_______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, QK_BOOT,
         _______, _______, _______, KC_MS_U, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,           _______, _______, _______, _______, _______, _______, KC_MPLY,
         _______, _______, _______, _______, _______, _______,                             _______, _______, KC_MPRV, KC_MNXT, _______, _______,
-        _______, _______, _______, KC_BTN1, KC_BTN2,         _______,            _______,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______,
+        _______, _______, _______, KC_BTN1, KC_BTN2,          _______,           _______,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______,
                                             _______, _______, _______,           _______, _______, _______
+    ),
+
+    [LEAG] = LAYOUT_moonlander(
+        KC_P,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             _______, _______, _______, _______, _______, _______, _______,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_A,    _______, KC_D,    KC_F,    KC_G,    KC_H,              _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_C,    _______, KC_B,                                _______, _______, _______, _______, _______, _______, 
+        KC_LCTL, _______, _______, KC_P,    KC_LALT,          KC_ESC,            _______,          _______, _______, _______, _______, _______,
+                                            KC_SPC,  KC_2,    KC_MUTE,           _______, _______, _______
     ),
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch (keycode) {
-        case VRSN:
-            SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-            return false;
-        }
-    }
-    return true;
-}
